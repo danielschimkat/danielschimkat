b7337863e5dffbc98bbfa2a5e70ec04ab6a5c25c
Daniel <daniel@daniel.fritz.box>
2020-09-30 00:04:28 +0200

    Typeanimation im bereich der Vorstellung

---
+++ src/js/function.js
@@ -0,0 +1,8 @@
+import Typed from 'typed.js';
+
+var typed = new Typed('#typed', {
+    strings: ['Electrical Engineer','Coder', 'Tinkerer' ],
+    typeSpeed: 80,
+    backDelay: 750,
+    loop: true
+});
