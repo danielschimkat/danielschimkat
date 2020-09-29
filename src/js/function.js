436d7eef3f716204b41d1287616a7f217253c130
Daniel <daniel@daniel.fritz.box>
2020-09-30 00:15:52 +0200

    kleine verbesserung am Typescript Teil "a, an" Fehler

--- src/js/function.js
+++ src/js/function.js
@@ -1,7 +1,7 @@
 import Typed from 'typed.js';
 
 var typed = new Typed('#typed', {
-    strings: ['Electrical Engineer','Coder', 'Tinkerer' ],
+    strings: ['An Electrical Engineer','A Coder', 'A Tinkerer' ],
     typeSpeed: 80,
     backDelay: 750,
     loop: true
