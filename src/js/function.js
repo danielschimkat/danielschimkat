244706a60a09a75dffa3ff1e4e492c373fe4ecb0
Daniel <daniel@daniel.fritz.box>
2020-10-04 02:41:26 +0200

    Scrollanimation mit Logo erstellt und scrollindikator hinzugefügt. Typedjs animation auskommentiert.

--- src/js/function.js
+++ src/js/function.js
@@ -1,4 +1,43 @@
-import Typed from 'typed.js';
+// Definition der Elemente
+const SVGLOGO = document.getElementById('logo');
+const ARROW = document.getElementsByClassName('arrow');
+
+//Definition Variablen
+const LOGOLENGTH = SVGLOGO.getTotalLength();
+
+//Basic Settings
+SVGLOGO.style.strokeDasharray = LOGOLENGTH;
+SVGLOGO.style.strokeDashoffset = LOGOLENGTH;
+
+//Funktion zum zeichnen des Pfads
+const drawWhenScroll = () => {
+    const DRAWLOGO = LOGOLENGTH * calcScrollPercent();
+    SVGLOGO.style.strokeDashoffset = LOGOLENGTH - DRAWLOGO;
+    if(calcScrollPercent() === 1){
+        SVGLOGO.style.fill = '#ED8936';
+    } else {
+        SVGLOGO.style.fill = 'transparent';
+    }
+    if(calcScrollPercent() <= 0.2){
+        ARROW.style.opacity = '1';
+    } else {
+        ARROW.style.opacity = '0';
+    }
+
+}
+
+//Funktion um %-Wert zu errechnen
+const calcScrollPercent = () => {
+    let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    return document.documentElement.scrollTop/height;
+}
+
+//Scroll-trigger
+window.addEventListener('scroll', drawWhenScroll);
+
+
+
+/*import Typed from 'typed.js';
 
 var typed = new Typed('#typed', {
     strings: ['An Electrical Engineer','A Coder', 'A Tinkerer' ],
@@ -6,3 +45,4 @@ var typed = new Typed('#typed', {
     backDelay: 750,
     loop: true
 });
+*/
